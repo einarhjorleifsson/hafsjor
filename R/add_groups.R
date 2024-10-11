0f316bb1d954ef679bbd1938c2c6789e473b062f
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2024-10-11 09:26:12 +0000

    leaflet explorations

---
+++ R/add_groups.R
@@ -0,0 +1,79 @@
+#' Add basebap
+#'
+#' @param map a map widget object created from leaflet
+#' @param tiles What layers - NOT YET ACTIVE
+#'
+#' @return
+#' @export
+#'
+#' @examples
+hs_add_base <- function(map, tiles) {
+  map |>
+  addTiles(urlTemplate = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+         attribution = 'Data source: <a href="https://www.hafogvatn.is">Marine & Freshwater Research Institute</a>',
+         group = "Hnöttur") |>
+  addTiles(urlTemplate = "https://heima.hafro.is/~einarhj/tiles/haf/050m/{z}/{x}/{-y}.png",
+           group = "Botnlag",
+           options = tileOptions(minZoom = 5, maxZoom = 16)) |>
+  addTiles(urlTemplate = "https://heima.hafro.is/~einarhj/tiles/haf/020m/{z}/{x}/{-y}.png",
+           group = "Botnlag",
+           options = tileOptions(minZoom = 5, maxZoom = 16)) |>
+  addTiles(urlTemplate = "https://heima.hafro.is/~einarhj/tiles/lhg/{z}/{x}/{-y}.png",
+           group = "Botnlag",
+           options = tileOptions(minZoom = 0, maxZoom = 16)) |>
+  addTiles(urlTemplate = "https://heima.hafro.is/~einarhj/tiles/vestfirdir_dpi/{z}/{x}/{-y}.png",
+           group = "Botnlag",
+           options = tileOptions(minZoom = 0, maxZoom = 16))
+}
+
+
+hs_add_veidar <- function(map) {
+  map |>
+    addWMSTiles(baseUrl = "https://gis.hafogvatn.is/geoserver/hafro/wms",
+                layers = "hafro:trawl_3857_byte",
+                group = "Fish trawl",
+                options = WMSTileOptions(format = "image/png",
+                                         transparent  = TRUE,
+                                         crs = "EPSG:3057",
+                                         opacity = 1)) |>
+    addWMSTiles(baseUrl = "https://gis.hafogvatn.is/geoserver/hafro/wms",
+                layers = "hafro:shrimp_3857_byte",
+                group = "Shrimp trawl",
+                options = WMSTileOptions(format = "image/png",
+                                         transparent  = TRUE,
+                                         #crs = "EPSG:3057",
+                                         opacity = 1)) |>
+    addWMSTiles(baseUrl = "https://gis.hafogvatn.is/geoserver/hafro/wms",
+                layers = "hafro:nephrops_3857_byte",
+                group = "Nephrops trawl",
+                options = WMSTileOptions(format = "image/png",
+                                         transparent  = TRUE,
+                                         crs = "EPSG:3057",
+                                         opacity = 1)) |>
+    addWMSTiles(baseUrl = "https://gis.hafogvatn.is/geoserver/hafro/wms",
+                layers = "hafro:longline_3857_byte",
+                group = "Longline",
+                options = WMSTileOptions(format = "image/png",
+                                         transparent  = TRUE,
+                                         crs = "EPSG:3057",
+                                         opacity = 1))
+}
+
+hs_add_log <- function(map) {
+  map |>
+    addWMSTiles(baseUrl = "https://gis.lmi.is/geoserver/wms",
+                layers = "LHG:landhelgi_12_milur_lina",
+                group = "12 mílur",
+                options = WMSTileOptions(format = "image/png",
+                                         transparent  = TRUE,
+                                         crs = "EPSG:3057",
+                                         opacity = 1)) |>
+    addWMSTiles(baseUrl = "https://gis.lmi.is/geoserver/wms",
+                layers = "LHG:efnahagslogsaga_lina",
+                group = "EEZ",
+                options = WMSTileOptions(format = "image/png",
+                                         transparent  = TRUE,
+                                         crs = "EPSG:3057",
+                                         opacity = 1))
+
+}
