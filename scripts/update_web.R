a5a7a74245608b7dc476357b77af5ef8151e8ab5
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2024-10-11 09:57:58 +0000

    first go

---
+++ scripts/update_web.R
@@ -0,0 +1,3 @@
+system("rm -rf /net/hafri.hafro.is/export/home/hafri/einarhj/public_html/hafsjor/*")
+system("cp -r _site/* /net/hafri.hafro.is/export/home/hafri/einarhj/public_html/hafsjor/.")
+system("chmod -R a+rX /net/hafri.hafro.is/export/home/hafri/einarhj/public_html/hafsjor")
